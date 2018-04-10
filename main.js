18f342b02aca03352636392e9306c5d141a57567
mohamed shkrui <mohamedshkry188@gmail.com>
2018-04-10 17:31:46 +0200

    jq

---
+++ main.js
@@ -0,0 +1,7 @@
+/* global $ , alert , console ,*/
+$(function(){
+   $("#click1").click(function(){
+   	  $("#demo").hide(8000);
+   	  $("#test").show(8000);
+   })
+});
---
+++ main1.js
@@ -0,0 +1,12 @@
+// var x  = 30;
+// 	y  = 70;
+// 	z1 = 0 ;
+// 	z2 = 0 ;
+// 	z3 = 0 ;
+// 	z4 = 0 ;
+// console.log(z1=x+y);
+// console.log(z2=x-y);
+// console.log(z3=x*y);
+// console.log(z4=x/y);
+// console.log(z1 < z2);
+// console.log(z3 == z4);
